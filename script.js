3a657b3ffa02588992254aa995f6814e89d76634
bitchard <richard@agreekhouse.com>
2022-06-26 11:35:26 -0500

    added the html for the special navabar. It will be like a pull cord that when clicked/pulled it will bring down navbar menu then when clicked or pulled again it will make navbar disapear.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change');
+    })
+})
+
+
 const icons = document.querySelectorAll('.section-1-icons i')
 let i = 1
 // we will use set interval it allows us to run the function iver and over again with some intervals. it takes 2 arguments//
 
 setInterval(() => {
         i++
+        const icon = document.querySelector('.section-1-icons.change')
+        icon.classList.remove('change')
 
-    const icon = document.querySelector('.section-1-icons.change')
-
-    icon.classList.remove('change')
-
-    if(i > icons.length){
+    if(i > icons.length) {
         icons[0].classList.add('change')
         i = 1
     } else {
         icon.nextElementSibling.classList.add('change')
     }
-
 }, 4000)
